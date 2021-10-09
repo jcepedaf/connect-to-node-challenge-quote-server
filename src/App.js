d9aea111f42fc104b91c0e61b20971c273f8f912
Jesus Cepeda <jcepedaf@gmail.com>
2021-10-09 13:44:31 +0200

    agrego React app as a front-end para conectar con api local

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [searchRandom, setSearchRandom] = useState();
+  const [searchInput, setSearchInput] = useState();
+  
+  const randomView = () =>{
+    fetch("http://localhost:5000/quotes/random")
+      .then(res => res.json())
+      .then(data => setSearchRandom(data));
+  }
+
+    const selectView = () =>{
+      fetch(`http://localhost:5000/quotes/search?term=${searchInput}`)
+      .then(res => res.json())
+      .then(data => data.length>0 ? setSearchRandom(data[0]) : setSearchRandom({"quote":"No Results", "author":""}));
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="input-group p-5">
+      <input type="text" className="form-control m-5" placeholder="Search Term" onChange={(e)=> setSearchInput(e.target.value)} />
+      <button className ="btn btn-outline-secondary m-5" type ="button" onClick={selectView}>Search</button>
+      <button className ="btn btn-outline-secondary m-5" type ="button" onClick={randomView}>Random</button>
+      {searchRandom? <div><h1>{searchRandom.quote}</h1><h1>{searchRandom.author}</h1></div> : null}
     </div>
   );
 }
-
 export default App;
